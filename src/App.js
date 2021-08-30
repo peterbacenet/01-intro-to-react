1d34c1bdacbf28eeb1955e4b3473e9d010ad0230
Peter Bacenet <peterbacenet@icloud.com>
2021-08-30 16:38:04 -0400

    Restructured for JXXSFormat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
+function ToyComponent() {
+  return (
+    <div id="toy-header">
+    <img
+      src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+      alt="toy-header"
+    />
+  </div>
+  )
+}
+function ContainerComponent() {
+  return (
+    <div className="container">
+    <form className="add-toy-form">
+      <h3>Create a toy!</h3>
+      <input
+          type="text"
+          name="name"
+          value=""
+          placeholder="Enter a toy's name..."
+          className="input-text"
+        />
+        <br />
+        <input
+          type="text"
+          name="image"
+          value=""
+          placeholder="Enter a toy's image URL..."
+          className="input-text"
+        />
+        <br />
+        <input
+          type="submit"
+          name="submit"
+          value="Create Toy"
+          className="submit"
+        />
+      </form>
+    </div>
+  )
+}
 function App() {
-  return <h1>Toys go here</h1>;
+  return (
+  <div>
+      {/* ToyComponent() */}
+        <ToyComponent />
+      {/* ContainerComponent() */}
+        <ContainerComponent />
+    <p style = {{textAlign:"center"}}>
+      Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
+    </p>
+    <div id="toy-collection"></div>
+    </div>
+    );
 }
 
 /* 
